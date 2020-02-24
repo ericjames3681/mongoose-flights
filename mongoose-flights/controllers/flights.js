5595448e54e9a0403b236d2463fc64e8726ce9e8
apple <apple@MacBook-4.local>
2020-02-24 15:42:31 -0600

    "changes..."

---
+++ mongoose-flights/controllers/flights.js
@@ -0,0 +1,18 @@
+const Flight = require('../models/flight');
+
+module.exports = {
+    index, 
+    show,
+    new: newFlight,
+    create
+}
+
+function newFlight(req, res) {
+    res.render('flights/new')
+}
+
+function create(req, res) {
+    Flight.create(req.body);
+    res.redirect('./flights/new');
+}
+
--- mongoose-flights/routes/flights.js
+++ mongoose-flights/routes/flights.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const flightsCtrl = require('./controllers/flights');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// GET /flights/new & ditch the annonymous inline function, replace it with flightsCtrl.new
+router.get('/new', flightsCtrl.new);
+
+//POST /movies
+router.post('/', flightsCtrl.create);
 
 module.exports = router;
