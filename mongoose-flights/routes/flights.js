c03ccf3b2aff1c61616f6f952371bb5778ef0acd
apple <apple@MacBook-4.local>
2020-02-24 16:01:30 -0600

    "Committing4..."

--- mongoose-flights/routes/flights.js
+++ mongoose-flights/routes/flights.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const flightsCtrl = require('./controllers/flights');
+const flightsCtrl = require('../controllers/flights');
 
 // GET /flights/new & ditch the annonymous inline function, replace it with flightsCtrl.new
 router.get('/new', flightsCtrl.new);
